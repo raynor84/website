2372d843c8f83d295a8faa2bd8c15b2a6a96f91f
Stefan Fischer <stefanfischer@stefan.local>
2022-12-12 15:22:43 +0000

    added skills, redesign
    changed to ignorechanged to ignore .DS_Store

--- js/link.js
+++ js/link.js
@@ -1,23 +1,22 @@
 
 function scrollToAnchor(aid){
     var aTag = $("[name='"+ aid +"']");
-    $('html,body').animate({scrollTop: aTag.offset().top-$("nav").height()},'slow');
+    $('html,body').stop(true, true).delay(500).animate({scrollTop: aTag.offset().top},'slow');
 }
 $(document).ready(function(){
 
 	$(".anchorlink").click(function(e) {
 		e.preventDefault();
-	   	myFunction();
+	   	closeNav();
 		var aid = $(this).attr("href");
 	   	scrollToAnchor(aid.substring(1));
 	});	
 });
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("navbar");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
-  }
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
 }
